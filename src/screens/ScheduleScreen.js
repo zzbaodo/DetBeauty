40a1c9e0ef44e39fe87e5efca4146399796ccc30
Bao <zzbaodo@gmail.com>
2020-12-20 03:15:22 -0800

    Fixed Scheduling Page

--- src/screens/ScheduleScreen.js
+++ src/screens/ScheduleScreen.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react"
+import { Form, Container } from "react-bootstrap"
 import firebase from "../firebase"
+import InfoIcon from "@material-ui/icons/Info"
 const ScheduleScreen = () => {
   const db = firebase.firestore()
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
   const [message, setMessage] = useState("")
   const onSubmitHandler = async (e) => {
     e.preventDefault()
@@ -21,34 +24,69 @@ const ScheduleScreen = () => {
     console.log("submit!")
   }
   return (
-    <div>
-      <form onSubmit={onSubmitHandler}>
-        <label htmlFor="name">
-          <input
-            value={name}
-            name="name"
+    <div className="scheduleForm-container">
+      <Form>
+        <Form.Group controlId="exampleForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
             placeholder="Name"
+            value={name}
             onChange={(e) => setName(e.target.value)}
           />
-        </label>
-        <label htmlFor="email">
-          <input
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlInput2">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
             value={email}
-            name="email"
-            placeholder="Email"
             onChange={(e) => setEmail(e.target.value)}
           />
-        </label>
-        <label htmlFor="message">
-          <textarea
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlInput3">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="xxx-xxx-xxxx"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>Services</Form.Label>
+          <Form.Control as="select">
+            <option>Ombre Powder Brows</option>
+            <option>Baby Lip Lush</option>
+            <option>Dark Lip Revitalization</option>
+            <option>Restorative Lip Blush</option>
+            <option>Lash Line Enhancement</option>
+            <option>Classic Liner</option>
+            <option>Smokey Liner</option>
+          </Form.Control>
+          <div style={{ display: "flex", alignContent: "center", fontSize:'12px' }}>
+            <InfoIcon style={{ fill: "#d4d4d4" }} />
+            <p className="d-block">
+              Note: If you would like to schedule for multiple services per
+              visit, please contact us via our email:{" "}
+              <a href="mailto:detbeautycomp@gmail.com" style={{textDecoration:'underline'}}>
+                detbeautycomp@gmail.com
+              </a>
+            </p>
+          </div>
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Additional Comments</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Optional"
             value={message}
-            name="message"
-            placeholder="Message"
             onChange={(e) => setMessage(e.target.value)}
           />
-        </label>
-        <button>Submit</button>
-      </form>
+        </Form.Group>
+      </Form>
     </div>
   )
 }
