6059e96443bb42c23811d0146c5dca54b98c6a45
Bao <zzbaodo@gmail.com>
2021-02-14 00:44:43 -0800

    update firestore

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase"
 import "firebase/firestore"
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC9FrCQhND73z631CPz6fBMebLHFHkhgO8",
-  authDomain: "dbeauty-95971.firebaseapp.com",
-  projectId: "dbeauty-95971",
-  storageBucket: "dbeauty-95971.appspot.com",
-  messagingSenderId: "287915579630",
-  appId: "1:287915579630:web:02bc55d8d01f52754a3bff",
+  apiKey: "AIzaSyBKu7RoL_Kri2ZXfaeaUqfTc7EGk1uVBXA",
+  authDomain: "detbeauty-f6565.firebaseapp.com",
+  projectId: "detbeauty-f6565",
+  storageBucket: "detbeauty-f6565.appspot.com",
+  messagingSenderId: "1078288371950",
+  appId: "1:1078288371950:web:e2c78c4f47a9b80cd6a3ff",
 })
 
 export default firebase
