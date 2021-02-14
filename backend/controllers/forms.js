0e5c70ed0dd5a7f7ea87ced38f1216668527afd8
Bao <zzbaodo@gmail.com>
2021-02-14 00:39:06 -0800

    removed backend folder

--- backend/controllers/forms.js
+++
@@ -1,3 +0,0 @@
-export const consentForm = (req,res) =>{
-    const {firstName, lastName, age, dob, phone, email,address, oldEnough, drugUse, pregnancyOrNursing, diabeticOrEpileptic, disease, chemoOrRadio, skinConditions, medicalHistory, acknowledgement} = req.body
-}
--- backend/routes/formRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-
-const route = express.Router()
-
-
-route.post('/consentform', consentForm)
